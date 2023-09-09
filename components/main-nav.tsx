4d926fe9cf6fe547871b61e17061f7242c359f28
maiguel <itsenergyy05@gmail.com>
2023-09-09 12:42:40 -0400

    format somehow

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -2,23 +2,23 @@
 
 import * as React from "react"
 import Link from "next/link"
-import {useSelectedLayoutSegment} from "next/navigation"
-import {MainNavItem} from "@/types"
+import { useSelectedLayoutSegment } from "next/navigation"
+import { MainNavItem } from "@/types"
 import { siteConfig } from "@/config/site"
 import { Icons } from "@/components/icons"
 import { cn } from "@/lib/utils"
 
 interface MainNavProps {
-    items?: MainNavItem[]
-    children?: React.ReactNode
-  }
+  items?: MainNavItem[]
+  children?: React.ReactNode
+}
 
-export function MainNav({ items, children }: MainNavProps){
-    const segment = useSelectedLayoutSegment()
-    // const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false)
+export function MainNav({ items, children }: MainNavProps) {
+  const segment = useSelectedLayoutSegment()
+  // const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false)
 
-    return(
-        <div className="flex gap-6 md:gap-10">
+  return (
+    <div className="flex gap-6 md:gap-10">
       <Link href="/" className="hidden items-center space-x-2 md:flex">
         <Icons.logo />
         <span className="hidden font-bold sm:inline-block">
@@ -55,5 +55,5 @@ export function MainNav({ items, children }: MainNavProps){
         <MobileNav items={items}>{children}</MobileNav>
       )} */}
     </div>
-    )
+  )
 }
