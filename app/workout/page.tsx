582a2d9d368474f8f38b429a294b302bb8682bda
iEnergyy <44242768+iEnergyy@users.noreply.github.com>
2023-09-12 22:03:24 -0400

    feat: add prettier (#3)

--- app/workout/page.tsx
+++ app/workout/page.tsx
@@ -1,30 +1,35 @@
-import { MainNav } from "@/components/main-nav"
-import { Button, buttonVariants } from "@/components/ui/button"
-import { baseNavConfig } from "@/config/base-nav"
-import { SignedIn, UserButton, SignedOut } from "@clerk/nextjs"
-import Link from "next/link"
+import { MainNav } from "@/components/main-nav";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { baseNavConfig } from "@/config/base-nav";
+import { SignedIn, UserButton, SignedOut } from "@clerk/nextjs";
+import Link from "next/link";
 
 export const metadata = {
-    title: "Workout",
-}
+  title: "Workout",
+};
 
 export default function WorkoutPage() {
-    return (
-        <div className="flex min-h-screen flex-col">
-            <header className="container z-40 bg-background">
-                <div className="flex h-20 items-center justify-between py-6">
-                    <MainNav items={baseNavConfig.mainNav} />
-                    <nav>
-                        <SignedIn>
-                            <UserButton afterSignOutUrl="/" />
-                        </SignedIn>
-                        <SignedOut>
-                            <Link href="/sign-in" className={buttonVariants({ variant: "secondary", size: "sm" })}>Sign in</Link>
-                        </SignedOut>
-                    </nav>
-                </div>
-            </header>
-            <Button>Workout</Button>
+  return (
+    <div className="flex min-h-screen flex-col">
+      <header className="container z-40 bg-background">
+        <div className="flex h-20 items-center justify-between py-6">
+          <MainNav items={baseNavConfig.mainNav} />
+          <nav>
+            <SignedIn>
+              <UserButton afterSignOutUrl="/" />
+            </SignedIn>
+            <SignedOut>
+              <Link
+                href="/sign-in"
+                className={buttonVariants({ variant: "secondary", size: "sm" })}
+              >
+                Sign in
+              </Link>
+            </SignedOut>
+          </nav>
         </div>
-    )
+      </header>
+      <Button>Workout</Button>
+    </div>
+  );
 }
--- config/site.ts
+++ config/site.ts
@@ -1,4 +1,4 @@
-import { SiteConfig } from "../types"
+import { SiteConfig } from "../types";
 
 export const siteConfig: SiteConfig = {
   name: "Muscle Monitor",
@@ -10,4 +10,4 @@ export const siteConfig: SiteConfig = {
     twitter: "https://twitter.com/maiguelll",
     github: "https://github.com/iEnergyy/muscle-monitor",
   },
-}
+};
