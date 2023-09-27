bb4d8108c8a6f1c3d41dd7b2e47deb3215495bb7
maiguel <itsenergyy05@gmail.com>
2023-09-27 09:38:03 -0400

    changed overview component to be ssr

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,24 +1,24 @@
-"use client";
-
 import { MainNav } from "@/components/main-nav";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { baseNavConfig } from "@/config/base-nav";
 import { SignedIn, SignedOut, UserButton, useUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/api";
 import Link from "next/link";
 
 export const metadata = {
   title: "Overview",
 };
 
-export default function OverviewPage() {
-  const { isLoaded, isSignedIn, user } = useUser();
+export default async function OverviewPage() {
+  const user: User | null = await currentUser();
 
   // In case the user signs out while on the page.
   let authMsg = "";
-  if (!isLoaded || !isSignedIn) {
+  if (!user) {
     authMsg = "You need to log in to see something here";
   } else {
-    authMsg = `Hello, ${user.firstName} welcome to Muscle Monitor.`;
+    authMsg = `Hello, ${user?.firstName} welcome to Muscle Monitor.`;
   }
 
   return (
