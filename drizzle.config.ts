73e5b27efe3c3f52828200e8dbe56e91d9c225a0
iEnergyy <itsenergyy05@gmail.com>
2023-11-18 15:32:22 -0400

    format and drizzle conf change

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ dotenv.config({ path: ".env.local" });
 
 export default {
   schema: "./db/schema.ts",
-  out: "./drizzle",
+  out: "./db",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL ?? "No valid DB url",
   },
