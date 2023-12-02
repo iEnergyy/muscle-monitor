b8d2f3285167d48ac6282e1b6cb7da81c0ce65a7
iEnergyy <itsenergyy05@gmail.com>
2023-12-02 14:52:26 -0400

    refactor icon on search

--- components/exercise-gallery.tsx
+++ components/exercise-gallery.tsx
@@ -32,21 +32,7 @@ export function ExerciseGallery({
         </h1>
         <div className="flex items-center">
           <div className="relative mr-4">
-            <svg
-              className=" absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400"
-              fill="none"
-              height="24"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              width="24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="11" cy="11" r="8" />
-              <path d="m21 21-4.3-4.3" />
-            </svg>
+            <IconSearch className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400" />
             <Input
               className="w-full appearance-none bg-white pl-8 shadow-none dark:bg-gray-900"
               placeholder="Search exercises..."
@@ -106,5 +92,25 @@ export function ExerciseGallery({
         </main>
       </div>
     </section>
+  );
+}
+
+function IconSearch(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="11" cy="11" r="8" />
+      <path d="m21 21-4.3-4.3" />
+    </svg>
   );
 }
