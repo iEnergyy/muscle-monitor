232a065923548663595be4c292fde3db8ee0b227
iEnergyy <itsenergyy05@gmail.com>
2024-01-04 22:02:15 -0400

    add dialog

--- components/add-exercise-dialog.tsx
+++ components/add-exercise-dialog.tsx
@@ -29,13 +29,60 @@ import {
   MuscleGroup,
   getAllMuscleGroups,
 } from "@/services/musclegroup-service";
-import { useState } from "react";
-4;
+import { useEffect, useState } from "react";
+import * as z from "zod";
+import { toast } from "@/components/ui/use-toast";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { clear } from "console";
 
 interface AddExerciseDialogProps extends React.HTMLAttributes<HTMLDivElement> {
   allMuscleGroups: MuscleGroup[];
   allEquipment: Equipment[];
 }
+
+const exerciseFormSchema = z
+  .object({
+    name: z.string({ required_error: "Please add a name." }),
+    description: z.string({
+      required_error: "Please write a description.",
+    }),
+    muscle: z.string({
+      required_error: "Please select a muscle.",
+    }),
+    equipment: z.string({
+      required_error: "Please select an equipment.",
+    }),
+  })
+  .refine(
+    (data) => {
+      // Custom validator: Check if all fields are non-empty
+      return Object.values(data).every((value) => value !== "");
+    },
+    {
+      message: "All fields must be filled.",
+    },
+  );
+
+type ExerciseFormValues = z.infer<typeof exerciseFormSchema>;
+
+// This can come from your database or API.
+const defaultValues: Partial<ExerciseFormValues> = {
+  name: "",
+  description: "",
+  muscle: "",
+  equipment: "",
+};
 
 export function AddExerciseDialog({
   allMuscleGroups,
@@ -50,105 +97,170 @@ export function AddExerciseDialog({
     setExerciseDescription("");
   };
 
-  return (
-    <Dialog onOpenChange={clearInputs}>
-      <DialogTrigger asChild>
-        <Button className="mr-2 rounded px-4 py-2 font-bold">
-          Add Exercise
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Add Exercise</DialogTitle>
-          <DialogDescription>
-            Make changes to add new exercises here. Click add when you are done.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              placeholder="New exercise name"
-              value={exerciseName}
-              onChange={(e) => setExerciseName(e.target.value)}
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="exercise" className="text-right">
-              Description
-            </Label>
-            <Input
-              id="description"
-              placeholder="Exercise description"
-              value={exerciseDescription}
-              onChange={(e) => setExerciseDescription(e.target.value)}
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="muscle" className="text-right">
-              Muscle
-            </Label>
-            <Select>
-              <SelectTrigger className="col-span-3">
-                <SelectValue placeholder="Select a muscle" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Muscles</SelectLabel>
-                  {allMuscleGroups.map((muscleGroup) => (
-                    <SelectItem
-                      key={muscleGroup.id}
-                      value={muscleGroup.id.toString()}
-                    >
-                      {muscleGroup.name}
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="equipment" className="text-right">
-              Equipment
-            </Label>
-            <Select>
-              <SelectTrigger className="col-span-3">
-                <SelectValue placeholder="Select the equipment" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Equipment</SelectLabel>
-                  {allEquipment.map((muscleGroup) => (
-                    <SelectItem
-                      key={muscleGroup.id}
-                      value={muscleGroup.id.toString()}
-                    >
-                      {muscleGroup.name}
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
+  const form = useForm<ExerciseFormValues>({
+    resolver: zodResolver(exerciseFormSchema),
+    defaultValues,
+    mode: "onChange",
+  });
+
+  const { formState } = form; // Destructure formState from the useForm hook
+  const isFormEmpty = Object.values(formState.dirtyFields).every(
+    (isDirty) => !isDirty,
+  );
+
+  function onSubmit(data: ExerciseFormValues) {
+    console.log(data);
+    clearInputs();
+    form.reset();
+
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <div className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
         </div>
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button type="submit" onClick={clearInputs}>
-              Add
-            </Button>
-          </DialogClose>
-          <DialogClose asChild>
-            <Button type="button" onClick={clearInputs} variant="secondary">
-              Cancel
-            </Button>
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <Dialog onOpenChange={clearInputs}>
+        <DialogTrigger asChild>
+          <Button className="mr-2 rounded px-4 py-2 font-bold">
+            Add Exercise
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <DialogHeader>
+              <DialogTitle>Add Exercise</DialogTitle>
+              <DialogDescription>
+                Make changes to add new exercises here. Click add when you are
+                done.
+              </DialogDescription>
+            </DialogHeader>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      id="name"
+                      placeholder="New exercise name"
+                      className="col-span-3"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input
+                      id="description"
+                      placeholder="Exercise description"
+                      className="col-span-3"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="muscle"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Muscle</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="col-span-3">
+                        <SelectValue placeholder="Select a muscle" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Muscles</SelectLabel>
+                        {allMuscleGroups.map((muscleGroup) => (
+                          <SelectItem
+                            key={muscleGroup.id}
+                            value={muscleGroup.id.toString()}
+                          >
+                            {muscleGroup.name}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="equipment"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Equipment</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="col-span-3">
+                        <SelectValue placeholder="Select the equipment" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Equipment</SelectLabel>
+                        {allEquipment.map((muscleGroup) => (
+                          <SelectItem
+                            key={muscleGroup.id}
+                            value={muscleGroup.id.toString()}
+                          >
+                            {muscleGroup.name}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter className="p-2">
+              <DialogClose asChild>
+                <Button
+                  type="submit"
+                  disabled={!formState.isValid || isFormEmpty}
+                  onClick={clearInputs}
+                >
+                  Add
+                </Button>
+              </DialogClose>
+              <DialogClose asChild>
+                <Button type="button" variant="secondary" onClick={clearInputs}>
+                  Cancel
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </Form>
   );
 }
--- services/exercise-service.ts
+++ services/exercise-service.ts
@@ -1,7 +1,25 @@
 import { db } from "@/db/config";
 import { exercise } from "@/db/schema";
-import { InferSelectModel } from "drizzle-orm";
+import { InferSelectModel, eq } from "drizzle-orm";
 
 export type Exercise = InferSelectModel<typeof exercise>;
 
 export const getAllExercise: Exercise[] = await db.select().from(exercise);
+
+export async function createExercise(newExercise: Exercise) {
+  // Check if an exercise with the same name already exists
+  const existingExercise = await db
+    .select()
+    .from(exercise)
+    .where(eq(exercise.name, newExercise.name!));
+
+  if (existingExercise) {
+    // Exercise with the same name already exists, handle it accordingly
+    throw new Error(
+      `Exercise with the name '${newExercise.name}' already exists.`,
+    );
+  }
+
+  // No duplicate, proceed with insertion
+  return db.insert(exercise).values(newExercise);
+}
